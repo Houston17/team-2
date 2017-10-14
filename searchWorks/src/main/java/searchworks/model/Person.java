5a5fc74c6942a2dda15c1745d2f41acd1d6604bb
Dan.Tor <danielx0@hotmail.com>
2017-10-14 05:13:52 -0500

    This is Kevins edit on my end
    This is kevins edit on my branch so I can get some edits of my own underway

---
+++ searchWorks/src/main/java/searchworks/model/Person.java
@@ -0,0 +1,71 @@
+package searchworks.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class Person {
+
+	@Id
+	@GeneratedValue(strategy= GenerationType.AUTO)
+	@Column
+	private Integer id;
+	
+	@Column
+	private String firstName;
+	
+	@Column
+	private String lastName;
+	
+	@Column
+	private String email;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+	}
+	
+	
+	
+	
+	
+}
---
+++ searchWorks/src/test/java/searchworks/SearchWorksApplicationTests.java
@@ -0,0 +1,16 @@
+package searchworks;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SearchWorksApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
